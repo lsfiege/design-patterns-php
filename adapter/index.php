3e1099faf93acc5398231563129608d31faf85fb
Lucas Fiege <lsfiege@gmail.com>
2021-10-08 18:36:14 -0300

    Isolated our domain logic from outside implementation

--- adapter/index.php
+++ adapter/index.php
@@ -7,12 +7,43 @@ class LocalNewsController extends Controller
         $locator = new IpLocation;
         $location = $locator->locate($request->ip());
 
-        /*
-         * Here we are being forced to adapt our domain logic to 3rd party package
-         * Literally we're gluing ourselves to their implementation
-         */
-        $news = News::near($location)->take(5)->get();
+        $mark = new Mark(
+            $location['country_name'],
+            $location['region_name'],
+            $location['city']
+        );
+
+        $news = News::near($mark)->take(5)->get();
 
         return NewsResource::collection($news);
     }
 }
+
+/*
+ * What happens if we need to switch the locator implementation?
+ * For example, getting the location using IP locator package is expensive,
+ * so the team buy a IP Database dataset
+ */
+
+ class LocalNewsController extends Controller
+ {
+     public function index(Request $request)
+     {
+         $locator = new IpDatabase;
+         $location = $locator->findByIpAddress($request->ip());
+
+         $mark = new Mark(
+             $location['country'],
+             $location['state_or_province'],
+             $location['city_name']
+         );
+
+         $news = News::near($mark)->take(5)->get();
+
+         return NewsResource::collection($news);
+     }
+ }
+
+ /*
+  * Here we detect that we can use Adapter Pattern to isolate our domain logic from outside influence
+  */
