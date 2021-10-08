59af7af19d46ce2b61060344b5bf192c53fcb0a3
Lucas Fiege <lsfiege@gmail.com>
2021-10-08 18:59:42 -0300

    Implemented adapter pattern

--- adapter/index.php
+++ adapter/index.php
@@ -1,17 +1,63 @@
 <?php
-
-class LocalNewsController extends Controller
+interface Locator
 {
-    public function index(Request $request)
+    public function fromIp(string $ipAddress): Mark;
+}
+
+class IpLocationLocator implements Locator
+{
+    public function fromIp(string $ipAddress): Mark
     {
         $locator = new IpLocation;
-        $location = $locator->locate($request->ip());
+        $location = $locator->locate($ipAddress);
 
-        $mark = new Mark(
+        return new Mark(
             $location['country_name'],
             $location['region_name'],
             $location['city']
         );
+    }
+}
+
+class IpDatabaseLocator implements Locator
+{
+    public function fromIp(string $ipAddress): Mark
+    {
+        $locator = new IpDatabase;
+        $location = $locator->findByIpAddress($ipAddress);
+
+        return new Mark(
+            $location['country'],
+            $location['state_or_province'],
+            $location['city_name']
+        );
+    }
+}
+
+class AppServiceProvider extends ServiceProvider
+{
+    // ...
+
+    public function register()
+    {
+        $this->app->singleton(Locator::class, function ($app) {
+            switch ($app->make('config')->get('services.ip-locator')) {
+                case 'api':
+                    return new IpLocationLocator;
+                case 'database':
+                    return new IpDatabaseLocator;
+                default:
+                    throw new \RuntimeException('Unknown IP Locator service');
+            }
+        });
+    }
+}
+
+class LocalNewsController extends Controller
+{
+    public function index(Request $request, Locator $locator)
+    {
+        $mark = $locator->fromIp($request->ip());
 
         $news = News::near($mark)->take(5)->get();
 
@@ -19,31 +65,52 @@ class LocalNewsController extends Controller
     }
 }
 
-/*
- * What happens if we need to switch the locator implementation?
- * For example, getting the location using IP locator package is expensive,
- * so the team buy a IP Database dataset
- */
+// In our tests
+class LocalNewsTest extends TestCase
+{
+    /** @test */
+    public function it_returns_only_local_news()
+    {
+        $response = $this->getJson('/api/local-news');
 
- class LocalNewsController extends Controller
- {
-     public function index(Request $request)
-     {
-         $locator = new IpDatabase;
-         $location = $locator->findByIpAddress($request->ip());
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}
 
-         $mark = new Mark(
-             $location['country'],
-             $location['state_or_province'],
-             $location['city_name']
-         );
+// This test can be reaching out over the network, so let's fix that
 
-         $news = News::near($mark)->take(5)->get();
+class LocalNewsTest extends TestCase
+{
+    /** @test */
+    public function it_returns_only_local_news()
+    {
+        $this->app->instance(
+            Locator::class,
+            FakeLocator::returning(new Mark('Canada', 'Ontario', 'Guelph'));
+        );
 
-         return NewsResource::collection($news);
-     }
- }
+        $response = $this->getJson('/api/local-news');
 
- /*
-  * Here we detect that we can use Adapter Pattern to isolate our domain logic from outside influence
-  */
+        $response->assertStatus(Response::HTTP_OK);
+    }
+}
+
+class FakeLocator implements Locator
+{
+    private $mark;
+
+    public function __construct(Mark $mark)
+    {
+        $this->mark = $mark;
+    }
+
+    public static function returning(Mark $mark)
+    {
+        return new static($mark);
+    }
+
+    public function fromIp(string $ipAddress): Mark
+    {
+        return $this->mark;
+    }
+}
